9a051d975d7500287f2ee6ae8bda201f69711d63
Fazal Mahmud Niloy <niloooy@gmail.com>
2020-05-24 05:44:41 +0600

    car game commented out.

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
-command = ""
-lastCommand = ""
-while True:
-    command = input(">").lower()
-    if command == "start":
-        if lastCommand == command:
-            print("the car has already started")
-        else:
-            print("car started...")
-            lastCommand = command
-    elif command == "stop":
-        if lastCommand == command:
-            print("the car has already STOPPED")
-        else:
-            print("car stopped.")
-            lastCommand = command
-    elif command == "quit":
-        break
-    elif command == "help":
-        print("""
-        start to start car.
-        stop to stop the car
-        quit to quit""")
+#  the car game START
+#
+# command = ""
+# lastCommand = ""
+# while True:
+#     command = input(">").lower()
+#     if command == "start":
+#         if lastCommand == command:
+#             print("the car has already started")
+#         else:
+#             print("car started...")
+#             lastCommand = command
+#     elif command == "stop":
+#         if lastCommand == command:
+#             print("the car has already STOPPED")
+#         else:
+#             print("car stopped.")
+#             lastCommand = command
+#     elif command == "quit":
+#         break
+#     elif command == "help":
+#         print("""
+#         start to start car.
+#         stop to stop the car
+#         quit to quit""")
+#  the car game END
+
