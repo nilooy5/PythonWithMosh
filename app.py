071d63189b009d644cd1716fc818b0be94b91b38
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-08-19 12:55:36 +0600

    new function added

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ from CarGame import car_game
 #car_game()
 # age_risk()
 
+def find_max(*argv):
+  return max(*argv)
+
+max = find_max(2, 4, 8, 3, 1, 33, 25, 65)
+
+print('Maximum value is ' + str(max))
+
 greet_user("Fazal Mahmud", "Niloy")
 greet_user("Munira", "Tabassum")
 emoji_converter("hello bros :) how are you :( sad? :D")
